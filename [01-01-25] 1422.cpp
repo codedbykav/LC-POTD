21541bd103a54745b1a331c9a18274666b05c55e
codedbykav <kavishakulshreshtha24@gmail.com>
2025-01-01 22:28:46 +0530

    Create [01-01-25] 1422.cpp
    Maximum score after splitting a string

---
+++ [01-01-25] 1422.cpp
@@ -0,0 +1,29 @@
+maximum-score-after-splitting-a-string
+
+
+class Solution {
+public:
+    int maxScore(string s) {
+               int onesCount = 0;
+        int zerosCount = 0;
+        int bestScore = INT_MIN;
+
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (s[i] == '1') {
+                onesCount++;
+            } else {
+                zerosCount++;
+            }
+            
+            bestScore = max(bestScore, zerosCount - onesCount);
+        }
+        
+        if (s[s.size() - 1] == '1') {
+            onesCount++;
+        }
+        
+        return bestScore + onesCount;
+ 
+    }
+};
+  
