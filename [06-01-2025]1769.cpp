f597af7405268e9a88d4ccf9f9e4a1d42a7a2d56
codedbykav <kavishakulshreshtha24@gmail.com>
2025-01-06 20:29:06 +0530

    Create [06-01-2025]1769.cpp

---
+++ [06-01-2025]1769.cpp
@@ -0,0 +1,23 @@
+//minimum-number-of-operations-to-move-all-balls-to-each
+
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        vector<int> pos, ans;
+        int len = boxes.size();
+        
+        for(int i = 0; i < len; i++)
+            if(boxes[i] == '1') 
+                pos.push_back(i);
+        
+        for(int i = 0; i < len; i++) {
+            int sum = 0;
+            for(int idx : pos) {
+                int dst = abs(i - idx);
+                sum += dst;
+            }
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
